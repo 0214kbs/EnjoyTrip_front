ceba43846e8421d33842f50d63f4e1be84b8c401
0214kbs <0214kbs@naver.com>
2023-05-17 09:41:33 +0900

    page

--- src/components/routers/routers.js
+++ src/components/routers/routers.js
@@ -3,4 +3,8 @@ import VueRouter from "vue-router"
 
 Vue.use(VueRouter);
 
+import BoardMain from "@/components/BoardMain.vue";
+import Login from "@/components/LoginPage.vue";
 
+import MapPage from "@/components/MapPage.vue";
+import store from "@/store/store.js";
