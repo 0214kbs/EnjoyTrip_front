e61bdf0908da8ecf67397cd7e4fe0d82a1e275bb
0214kbs <0214kbs@naver.com>
2023-05-17 15:25:02 +0900

    map

--- src/components/MapPage.vue
+++ src/components/MapPage.vue
@@ -1,3 +1,54 @@
-<template></template>
-<script></script>
-<style scoped></style>
+<template>
+  <div class="col-md-9 mx-auto container mb-5">
+    <br /><br /><br /><br /><br /><br />
+    <div class="section-title">
+      <h2 style="margin-top: 20px">지역별 관광 정보</h2>
+      <form class="d-flex my-3">
+        <select id="area1List" class="form-select me-2">
+          <option value="">시도 선택</option>
+        </select>
+        <select id="area2List" class="form-select me-2">
+          <option value="">구군 선택</option>
+        </select>
+        <select id="cat1List" class="form-select me-2">
+          <option value="">대분류 선택</option>
+        </select>
+        <select id="cat2List" class="form-select me-2">
+          <option value="">중분류 선택</option>
+        </select>
+        <select id="cat3List" class="form-select me-2">
+          <option value="">소분류 선택</option>
+        </select>
+        <button type="button" class="btn btn-outline-success" id="btnSearch">검색</button>
+      </form>
+
+      <!-- kakao map start -->
+      <div id="map" class="mt-3" style="width: 100%; height: 400px"></div>
+      <!-- kakao map end -->
+
+      <!-- 관광지 table -->
+      <div id="table-container"></div>
+      <!-- End 관광지 table -->
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "kakaoMap",
+  data() {
+    return {};
+  },
+  setup() {},
+  created() {},
+  mounted() {},
+  unmounted() {},
+  methods: {},
+};
+</script>
+
+<style scoped>
+body {
+  background-color: white;
+}
+</style>
